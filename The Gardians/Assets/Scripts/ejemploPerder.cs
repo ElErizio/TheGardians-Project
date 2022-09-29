009cd6c490ad605233f04c5580f14ea69a7c1659
ElErizio <theericyea100@hotmail.com>
2022-09-29 11:21:09 -0500

    Se actualizó el ejemplo de GameOver
    Se actualizó el script del game over y se agregaron escenas

---
+++ The Gardians/Assets/Scripts/ejemploPerder.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ejemploPerder : MonoBehaviour
+{
+    private int CantidadEnemigos; 
+    // Update is called once per frame
+    void Start()
+    {   
+        CantidadEnemigos = GameObject.FindGameObjectsWithTag("Enemy").Length;
+    }
+}
